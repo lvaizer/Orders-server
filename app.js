912be3de6543b933cbd7d8d47f61c9786652355e
Liron Vaizer <lvaizer@gmail.com>
2023-02-07 11:55:21 +0200

    test

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('./Constants');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
@@ -17,5 +16,11 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use('/orders', ordersRouter);
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage')
+})
+app.get('/test', (req, res) => {
+    res.send('GET request to the homepage test')
+})
 
 module.exports = app;
