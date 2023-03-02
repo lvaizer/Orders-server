187768c7678180aa2730a3e60dbe59c845859a38
Liron Vaizer <lvaizer@gmail.com>
2023-03-02 13:42:29 +0200

    fix

--- app.js
+++ app.js
@@ -9,9 +9,12 @@ const ordersRouter = require('./routes/orders');
 const app = express();
 const skip = (req, res) => res.statusCode <= 200;
 
-app.use(cors({
-    origin: '*'
-}));
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next();
+})
 
 app.use(logger('dev', {skip: skip}));
 app.use(express.json());
