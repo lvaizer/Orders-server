85a6926ed3d54d65a917d97be1bb6e52eaf311ab
Liron Vaizer <lvaizer@gmail.com>
2023-03-02 14:32:06 +0200

    fix

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ require('./Constants');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-// const cors = require('cors');
+const cors = require('cors');
 
 const ordersRouter = require('./routes/orders');
 
 const app = express();
 const skip = (req, res) => res.statusCode <= 200;
 
-// app.use(cors());
-app.use((req,res,next)=>{
-    console.log(req.headers.origin);
-    console.log(res.headers)
-    res.header("Access-Control-Allow-Method", "GET,POST");
-    next();
-})
+app.use(cors());
 app.use(logger('dev', {skip: skip}));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
