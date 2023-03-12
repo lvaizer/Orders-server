63103e401c8ad2f6557f2db1e5905c5bcac427d0
Liron Vaizer <lvaizer@gmail.com>
2023-03-12 20:38:30 +0200

    clean unnecessary code and add helmet package

--- app.js
+++ app.js
@@ -2,25 +2,20 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const ordersRouter = require('./routes/orders');
 
 const app = express();
 const skip = (req, res) => res.statusCode <= 200;
-
+app.use(helmet());
 app.use(cors({methods: "GET,POST"}));
 app.use(logger('dev', {skip: skip}));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use('/orders', ordersRouter);
-app.get('/', (req, res) => {
-    res.send('GET request to the homepage')
-})
 
-app.get('/test', (req, res) => {
-    res.send('GET request to the homepage test')
-})
+app.use('/orders', ordersRouter);
 
 const PORT = process.env.PORT || 3000;
 
